dfd2840ac48698ca193e983007418923890921c4
Yagudagu <Jacobhmartian@gmail.com>
2021-07-06 13:43:15 +0000

    test 1:43

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -11,6 +11,6 @@ import java.util.function.Function;
 public class Hello implements Function<Mono<User>, Mono<Greeting>> {
 
     public Mono<Greeting> apply(Mono<User> mono) {
-        return mono.map(user -> new Greeting("Hello and without run from package, " + user.getName() + "!\n"));
+        return mono.map(user -> new Greeting("Hello and cha cha cha cha chanages, " + user.getName() + "!\n"));
     }
 }
